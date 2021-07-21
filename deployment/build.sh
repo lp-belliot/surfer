f927326d73d338c43bb263d60519dbc1132b2494
Ben Elliot <belliot@liveperson.com>
2021-07-22 09:52:04 +1000

    Add build script

---
+++ deployment/build.sh
@@ -0,0 +1,8 @@
+#! /bin/bash 
+
+IMAGE="lpbelliot/surfer_static"
+VERSION="latest"
+SCRIPT_PATH="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+
+docker build -t ${IMAGE}:${VERSION} $SCRIPT_PATH/
+docker push ${IMAGE}:${VERSION}
