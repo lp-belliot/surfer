233515f5198198daed8f33507b82be8f03b43c50
Ben Elliot <belliot@liveperson.com>
2021-07-22 21:00:31 +1000

    Add build / deploy helpers

--- deployment/deploy.sh
+++ deployment/deploy.sh
@@ -5,3 +5,9 @@ VERSION="latest"
 SCRIPT_PATH="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 
 docker build -t ${IMAGE}:${VERSION} $SCRIPT_PATH/../
+docker push ${IMAGE}:${VERSION}
+
+kubectl rollout restart deployment \
+  lp-static-deployment \
+  cba-static-deployment \
+  -n surfer-static
