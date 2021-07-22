74c3e8326e5c18844b47ad72e75d76358b6f9df4
Ben Elliot <belliot@liveperson.com>
2021-07-22 12:01:40 +1000

    Update build script based on new file location

--- deployment/build.sh
+++ deployment/build.sh
@@ -4,5 +4,5 @@ IMAGE="lpbelliot/surfer_static"
 VERSION="latest"
 SCRIPT_PATH="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 
-docker build -t ${IMAGE}:${VERSION} $SCRIPT_PATH/
+docker build -t ${IMAGE}:${VERSION} $SCRIPT_PATH/../
 docker push ${IMAGE}:${VERSION}
